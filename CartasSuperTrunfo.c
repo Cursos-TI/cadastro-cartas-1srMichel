210114ef35872298977932def7cb1023b82151b7
Otavio M <142915864+1srMichel@users.noreply.github.com>
2026-01-28 14:12:17 +0000

    cd2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@
 
 int main() {
 
-  printf("SUPER TRUNFO \n Cadastro de cartas \n");
+  printf("SUPER TRUNFO\n Cadastro de cartas\n");
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   //variaveis da cidade 1
@@ -30,27 +30,52 @@ int main() {
   // Área para entrada de dados
 
   //da cidade 1
-  printf("cadastro da cidade 1: \n");
-  printf("insira o estado: \n");
+  printf("cadastro da cidade 1:\n");
+  printf("insira o estado:\n");
   scanf(" %c", estado1);
+  getchar();
 
-  printf("insira o codigo da cidade: \n");
+  printf("insira o codigo da cidade:\n");
   scanf(" %s", codigo1);
 
-  printf("insira o nome da cidade: \n");
+  printf("insira o nome da cidade:\n");
   scanf(" %s", nome1);
 
-  printf("insira a população da cidade 1: \n");
-  scanf("%i", populacao1);
+  printf("insira a população da cidade 1:\n");
+  scanf("%i", &populacao1);
 
-  printf("insira a área em km da cidade 1: \n");
-  scanf("%f", areakm1);
+  printf("insira a área em km da cidade 1:\n");
+  scanf("%f", &areakm1);
 
-  printf("insira o PIB dessa cidade: \n");
-  scanf("%i", pib1);
+  printf("insira o PIB dessa cidade:\n");
+  scanf("%i", &pib1);
 
-  printf("insira o numero de pontos turisticos da cidade 1: \n");
-  scanf("%i", pontosturisticos1);
+  printf("insira o numero de pontos turisticos da cidade 1:\n");
+  scanf("%i", &pontosturisticos1);
+
+  //da cidade 2
+  printf("cadastro da cidade 2:\n");
+  printf("insira o estado:\n");
+  scanf(" %c", estado2);
+  getchar();
+  
+  printf("insira o codigo da cidade:\n");
+  scanf(" %s", codigo2);
+
+  printf("insira o nome da cidade:\n");
+  scanf(" %s", nome2);
+
+  printf("insira a população da cidade 2:\n");
+  scanf("%i", &populacao2);
+
+  printf("insira a área em km da cidade 2:\n");
+  scanf("%f", &areakm2);
+
+  printf("insira o PIB dessa cidade:\n");
+  scanf("%i", &pib2);
+
+  printf("insira o numero de pontos turisticos da cidade 1:\n");
+  scanf("%i", &pontosturisticos2);
 
   // Área para exibição dos dados da cidade
 
